3a64fd5fcc1fcd250610afb4f946b9fe2c66208f
sainingo <nickmentor421@gmail.com>
2022-05-04 18:25:20 +0300

    Add all the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import BookList from './components/BookList';
+import Books from './components/Books';
+import Category from './components/Category';
+import FormList from './components/FormList';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Components</h1>
+      <Header />
+      <Books />
+      <BookList />
+      <FormList />
+      <Category />
     </div>
   );
 }
---
+++ src/components/FormList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormList = () => {
+  return (
+    <div>FormList</div>
+  )
+}
+
+export default FormList
