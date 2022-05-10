1cb4c4ab8bf3251b5e359972d41a75f02e296b78
sainingo <nickmentor421@gmail.com>
2022-05-10 17:29:14 +0300

    Fix conflict issue

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-const ADD_BOOK = 'ADD_BOOK';
-const REMOVE_BOOK = 'REMOVE_BOOK';
-
-export const addBook = (book) => ({ type: ADD_BOOK, payload: book });
-export const removeBook = (id) => ({ type: REMOVE_BOOK, payload: { id } });
-
-const books = (state = [], action) => {
-  switch (action.type) {
-    case ADD_BOOK:
-      return [...state, {
-        id: state.length,
-        title: action.payload.title,
-        author: action.payload.author,
-=======
 import initialData from './initialData';
 
 const ADD_BOOK = 'bookstore/src/redux/books/ADD_BOOK';
@@ -29,7 +14,6 @@ const books = (state = initialData, action) => {
         title: action.payload.title,
         author: action.payload.author,
         category: action.payload.category,
->>>>>>> react-redux-usage
       }];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload.id);
